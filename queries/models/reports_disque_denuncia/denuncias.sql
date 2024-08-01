97b561111f443b817748fb00198d7651ed5f6def
Nicolas Evilasio <nicolas_evilasio@hotmail.com>
2024-08-01 14:37:31 -0300

    docs:  add comments to model for clarity and maintainability and update docs in schema.

--- queries/models/reports_disque_denuncia/denuncias.sql
+++ queries/models/reports_disque_denuncia/denuncias.sql
@@ -1,6 +1,7 @@
 {{
     config(
         materialized='table',
+        unique_key='id_denuncia',
         partition_by={
             "field": "data_denuncia",
             "data_type": "date",
@@ -8,14 +9,14 @@
         }
     )
 }}
-
-
+-- Common Table Expression (CTE) to clean and format data from the `denuncias` table
 WITH denuncias_cte AS (
     SELECT
         CAST(denuncia_id AS INT) AS id_denuncia,
         denuncia_numero AS numero_denuncia,
         DATETIME(datetime_denuncia) AS data_denuncia,
         DATETIME(datetime_difusao) AS data_difusao,
+        -- Clean and format address fields
         TRIM(REGEXP_REPLACE(endereco, r'\s+', ' ')) AS endereco,
         TRIM(REGEXP_REPLACE(bairro, r'\s+', ' ')) AS bairro,
         TRIM(REGEXP_REPLACE(municipio, r'\s+', ' ')) AS municipio,
@@ -23,16 +24,23 @@ WITH denuncias_cte AS (
         CAST(latitude AS FLOAT64) AS latitude,
         CAST(longitude AS FLOAT64) AS longitude,
         relato,
+        -- Extract filename from the _FILE_NAME field using regex
         REGEXP_EXTRACT(_FILE_NAME, r'(\d{8}_\d{6}_\d{6})_report_disque_denuncia\.csv$') AS nome_arquivo
     FROM
         `dd-teste.disque_denuncia.denuncias`
-), denuncias_ranked AS (
+),
+
+-- CTE to rank records within each id_denuncia partition
+denuncias_ranked AS (
     SELECT
         *,
+        -- Assign a row number within each id_denuncia partition, ordered by nome_arquivo (descending) and data_difusao (descending)
         ROW_NUMBER() OVER (PARTITION BY id_denuncia ORDER BY nome_arquivo DESC, data_difusao DESC) AS ranking
     FROM
         denuncias_cte
 )
+
+-- Final selection of the top-ranked records (ranking = 1) for each id_denuncia
 SELECT
     id_denuncia,
     numero_denuncia,
