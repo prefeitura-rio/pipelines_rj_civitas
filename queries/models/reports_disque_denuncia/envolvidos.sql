36fae55cd4fc4406b3eb96930d421993557682b5
Nicolas Evilasio <nicolas_evilasio@hotmail.com>
2024-08-02 17:36:01 -0300

    refactor: replace multiple OR conditions with COALESCE in query. y Please enter the commit message for your changes. Lines starting

--- queries/models/reports_disque_denuncia/envolvidos.sql
+++ queries/models/reports_disque_denuncia/envolvidos.sql
@@ -21,13 +21,15 @@ SELECT DISTINCT
 FROM
     {{ref('stg_denuncias')}}
 WHERE
-    nome_envolvido IS NOT NULL
-    OR vulgo_envolvido IS NOT NULL
-    OR sexo_envolvido IS NOT NULL
-    OR idade_envolvido IS NOT NULL
-    OR pele_envolvido IS NOT NULL
-    OR estatura_envolvido IS NOT NULL
-    OR olhos_envolvido IS NOT NULL
-    OR cabelos_envolvido IS NOT NULL
-    OR porte_envolvido IS NOT NULL
-    OR outras_caracteristicas_envolvido IS NOT NULL
+    COALESCE(
+        nome_envolvido,
+        vulgo_envolvido,
+        sexo_envolvido,
+        CAST(idade_envolvido AS STRING),
+        pele_envolvido,
+        estatura_envolvido,
+        olhos_envolvido,
+        cabelos_envolvido,
+        porte_envolvido,
+        outras_caracteristicas_envolvido
+    ) IS NOT NULL
