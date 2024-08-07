2021747f0df4ef3ab45fec3a41f43c6643542e75
Nicolas Evilasio <nicolas_evilasio@hotmail.com>
2024-08-07 11:14:39 -0300

    fix: fix sql query in stg_denuncias model

--- queries/models/disque_denuncia/stg_denuncias.sql
+++ queries/models/disque_denuncia/stg_denuncias.sql
@@ -1,8 +1,12 @@
 {{
     config(
-        materialized='table',
+        materialized='ephemeral',
     )
 }}
+-- ATTENTION!
+-- THIS TABLE HAS DUPLICATED VALUES DUE TO THE FACT THAT A REPORT CAN HAVE MULTIPLES, ORGAOS, XPTOS, ASSUNTOS...
+-- YOU NEED TO USE "DISTINCT" OR OTHER WAY TO ENSURE UNIQUENESS WHEN REFERENCES THIS TABLE.
+-- USING THE RANKING = 1 WILL ENSURE THAT THE INFO IS THE NEWEST (THE MOST RECENT FILE FOUND IN BUCKET)
 WITH stg_denuncias AS (
     SELECT
         denuncia_numero AS numero_denuncia, -- STRING
@@ -13,8 +17,15 @@ WITH stg_denuncias AS (
         DATETIME(datetime_difusao) AS data_difusao,
         CAST(denuncia_imediata AS INT) AS denuncia_imediata,
         -- Clean and format address fields
-        TRIM(REGEXP_REPLACE(endereco, r'\s+', ' ')) AS endereco,
+        CAST(NULL AS STRING) AS tipo_logradouro, -- >>>>>> den_logr_tp > FALTA WOODY INSERIR NA API
+        -- for now, the "endereco" field returns the full address
+        TRIM(REGEXP_REPLACE(endereco, r'\s+', ' ')) AS logradouro, -- >>>>>> den_logr_ds > FALTA WOODY INSERIR NA API
+        CAST(NULL AS STRING) AS numero_logradouro, -- >>>>>> den_logr_num > FALTA WOODY INSERIR NA API
+        CAST(NULL AS STRING) AS complemento_logradouro,  -- >>>>>> den_logr_cmpl > FALTA WOODY INSERIR NA API
         TRIM(REGEXP_REPLACE(bairro, r'\s+', ' ')) AS bairro,
+        CAST(NULL AS STRING) AS subbairro, -- >>>>>> den_logr_subbairro > FALTA WOODY INSERIR NA API
+        CAST(NULL AS STRING) AS cep_logradouro,  -- >>>>>> den_logr_ds > FALTA WOODY INSERIR NA API
+        CAST(NULL AS STRING) AS referencia_logradouro, -- >>>>>> den_logr_ref > FALTA WOODY INSERIR NA API
         TRIM(REGEXP_REPLACE(municipio, r'\s+', ' ')) AS municipio,
         TRIM(estado) AS estado,
         CAST(latitude AS FLOAT64) AS latitude,
@@ -25,25 +36,29 @@ WITH stg_denuncias AS (
         CAST(orgao_id AS INT) AS id_orgao,
         orgao_nome AS nome_orgao, -- STRING
         orgao_tipo AS tipo_orgao, -- STRING
-        assunto_classe AS classe_assunto, -- STRING
-        assunto_tipo AS tipo_assunto, -- STRING
-        envolvido_nome AS nome_envolvido, -- STRING
-        envolvido_vulgo AS vulgo_envolvido, -- STRING
-        envolvido_sexo AS sexo_envolvido, -- STRING
+        CAST(NULL AS STRING) AS id_classe, -- >>>>>> cla_cd > FALTA WOODY INSERIR NA API
+        assunto_classe AS descricao_classe, -- STRING
+        CAST(NULL AS INT) AS assunto_principal, -- >>>>>> ass_principal > FALTA WOODY INSERIR NA API
+        CAST(NULL AS STRING) AS id_tipo, -- >>>>>> tpa_cd > FALTA WOODY INSERIR NA API
+        assunto_tipo AS descricao_tipo, -- STRING
+        CAST(NULL AS INT) AS id_envolvido, -- env_cd >>>>>> FALTA WOODY INSERIR NA API
+        TRIM(REGEXP_REPLACE(envolvido_nome, r'\s+', ' ')) AS nome_envolvido,
+        TRIM(REGEXP_REPLACE(envolvido_vulgo, r'\s+', ' ')) AS vulgo_envolvido,
+        TRIM(REGEXP_REPLACE(envolvido_sexo, r'\s+', ' ')) AS sexo_envolvido,
         CAST(envolvido_idade AS INT) AS idade_envolvido,
-        envolvido_pele AS pele_envolvido, -- STRING
-        envolvido_estatura AS estatura_envolvido, -- STRING
-        envolvido_porte AS porte_envolvido, -- STRING
-        envolvido_cabelos AS cabelos_envolvido,	 -- STRING
-        envolvido_olhos AS olhos_envolvido,	 -- STRING
-        envolvido_outras_caracteristicas AS outras_caracteristicas_envolvido,	 -- STRING
+        TRIM(REGEXP_REPLACE(envolvido_pele, r'\s+', ' ')) AS pele_envolvido,
+        TRIM(REGEXP_REPLACE(envolvido_estatura, r'\s+', ' ')) AS estatura_envolvido,
+        TRIM(REGEXP_REPLACE(envolvido_olhos, r'\s+', ' ')) AS olhos_envolvido,
+        TRIM(REGEXP_REPLACE(envolvido_cabelos, r'\s+', ' ')) AS cabelos_envolvido,
+        TRIM(REGEXP_REPLACE(envolvido_porte, r'\s+', ' ')) AS porte_envolvido,
+        TRIM(REGEXP_REPLACE(envolvido_outras_caracteristicas, r'\s+', ' ')) AS outras_caracteristicas_envolvido,
         denuncia_status AS status_denuncia,	 -- STRING
         REGEXP_EXTRACT(
             _FILE_NAME,
             r'(\d{8}_\d{6}_\d{6})_report_disque_denuncia\.csv$'
         ) AS nome_arquivo
     FROM
-         `rj-civitas.disque_denuncia_staging.denuncias`
+         `dd-teste.disque_denuncia.denuncias`
 ), denuncias_ranking AS (
     SELECT
         *,
