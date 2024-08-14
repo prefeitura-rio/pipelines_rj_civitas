95ffc01f8162111a6bd9f9b42f7dfe9db2107db7
Nicolas Evilasio <nicolas_evilasio@hotmail.com>
2024-08-14 17:57:07 -0300

    refactor: changing model name in flows.py and removing obsolete files.

--- pipelines/disque_denuncia/__init__.py
+++ pipelines/disque_denuncia/__init__.py
@@ -4,6 +4,4 @@ This module imports functions and classes from the 'pipelines.reports_disque_den
 module.
 """
 
-from pipelines.disque_denuncia.dump_historico.flows import *  # noqa
 from pipelines.disque_denuncia.extract.flows import *  # noqa
-from pipelines.disque_denuncia.materialize.flows import *  # noqa
