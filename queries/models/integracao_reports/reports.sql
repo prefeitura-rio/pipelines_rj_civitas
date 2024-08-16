aa23d14603bbc4bd3010df5b2dec944e630ce59d
Nicolas Evilasio <nicolas_evilasio@hotmail.com>
2024-08-15 23:37:33 -0300

    feat: model reports.sql created to materialize a table in integracao_reports dataset, combining data from multiples sources into an unique table.

---
+++ queries/models/integracao_reports/reports.sql
@@ -0,0 +1,25 @@
+{{
+  config(
+    materialized = 'table',,
+    unique_key='id_report',
+    partition_by={
+        "field": "data_report",
+        "data_type": "datetime",
+        "granularity": "hour",
+    }
+    )
+}}
+
+SELECT
+    CONCAT(id_source, id_report_original) AS id_report,
+    *
+FROM
+    {{ ref("stg_disque_denuncia_reports") }}
+
+UNION ALL
+
+SELECT
+    CONCAT(id_source, id_report_original) AS id_report,
+    *
+FROM
+    {{ ref("stg_1746_reports") }}
