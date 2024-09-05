377e4697df6413e87adfea1afa972785c7de6fb3
acsamaro <85083027+acsamaro@users.noreply.github.com>
2024-09-04 21:09:03 -0300

    feat(placas_clonadas): create dbt model for vehicle movement analysis

--- pipelines/placas_clonadas/materialize/flows.py
+++ pipelines/placas_clonadas/materialize/flows.py
@@ -48,7 +48,8 @@ with Flow(
 
     dump_prod_tables_to_materialize_parameters = [
         # {"dataset_id": dataset_id, "table_id": "radares_confiaveis", "dbt_alias": False},
-        {"dataset_id": dataset_id, "table_id": "dados_radares", "dbt_alias": False},
+        # {"dataset_id": dataset_id, "table_id": "dados_radares", "dbt_alias": False},
+        {"dataset_id": dataset_id, "table_id": "identificacao_anomalias_placas", "dbt_alias": False},
     ]
 
     dump_prod_materialization_flow_runs = create_flow_run.map(
