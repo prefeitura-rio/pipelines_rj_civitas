1d1c30339a63513db086de9b71c1c8d2ffefb057
acsamaro <85083027+acsamaro@users.noreply.github.com>
2024-09-05 15:47:53 -0300

    fix(placas_clonadas): wrong sql file upload

--- queries/models/placas_clonadas/dados_placas.sql
+++ queries/models/placas_clonadas/dados_placas.sql
@@ -1,63 +1,39 @@
 {{
   config(
-    materialized = 'incremental',
-    unique_key = 'placa',
-    partition_by = {
-        'field': 'datahora_inicial',
-        'data_type': 'timestamp'
-    },
-    cluster_by = ['placa', 'camera_inicial', 'camera_final']
+    materialized = 'table',
+    cluster_by = ['ano_mes', 'placa']
   )
 }}
 
-WITH movimentacao_placa AS (
-    SELECT inicio.placa, 
-        GREATEST(inicio.erro_datahora, fim.erro_datahora) AS erro_datahora,
-        inicio.tipoveiculo AS tipo_inicial,
-        inicio.velocidade AS velocidade_inicial,
-        inicio.camera_numero AS camera_inicial,
-        inicio.datahora AS datahora_inicial, 
-        ST_GEOGPOINT(inicio.longitude, inicio.latitude) AS posicao_inicial,
-        fim.tipoveiculo AS tipo_final,
-        fim.velocidade AS velocidade_final,
-        fim.camera_numero AS camera_final,
-        fim.datahora AS datahora_final,
-        ST_GEOGPOINT(fim.longitude, fim.latitude) AS posicao_final,
-        (inicio.tipo_confiavel AND fim.tipo_confiavel) tipo_confiavel
-    FROM `rj-civitas.placas_clonadas.dados_radares` inicio
-    INNER JOIN `rj-civitas.placas_clonadas.dados_radares` fim
-        ON inicio.placa = fim.placa
-        AND inicio.datahora < fim.datahora
-),
-velocidade_veiculo AS (
-  SELECT placa, GREATEST(150, MAX(velocidade) + 30) AS velocidade_maxima
+WITH primeira_ultima_camera_por_dia AS (
+  SELECT
+    placa,
+    EXTRACT(YEAR FROM datahora) AS ano,        -- Extrai o ano da data
+    EXTRACT(MONTH FROM datahora) AS mes,       -- Extrai o mês da data
+    ARRAY_AGG(camera_numero ORDER BY datahora ASC LIMIT 1)[OFFSET(0)] AS primeira_camera_dia, -- Primeira câmera do dia
+    ARRAY_AGG(camera_numero ORDER BY datahora DESC LIMIT 1)[OFFSET(0)] AS ultima_camera_dia    -- Última câmera do dia
   FROM `rj-civitas.placas_clonadas.dados_radares`
-  GROUP BY placa
+  GROUP BY placa, EXTRACT(YEAR FROM datahora), EXTRACT(MONTH FROM datahora), DATE(datahora)
+),
+
+mediana_cameras AS (
+  SELECT
+    placa,
+    CONCAT(CAST(ano AS STRING), '-', LPAD(CAST(mes AS STRING), 2, '0')) AS ano_mes,  -- Cria o campo ano_mes aqui
+    APPROX_QUANTILES(primeira_camera_dia, 100)[OFFSET(50)] AS mediana_primeira_camera,  -- Mediana das primeiras câmeras
+    APPROX_QUANTILES(ultima_camera_dia, 100)[OFFSET(50)] AS mediana_ultima_camera       -- Mediana das últimas câmeras
+  FROM primeira_ultima_camera_por_dia
+  GROUP BY placa, ano_mes, ano, mes
 )
 
-SELECT mp.placa,
-    mp.camera_inicial,
-    mp.camera_final,
-    mp.posicao_inicial, 
-    mp.posicao_final, 
-    mp.datahora_inicial, 
-    mp.datahora_final,
-    mp.velocidade_inicial,
-    mp.velocidade_final,
-    (mp.tipo_inicial != mp.tipo_final) AS tipos_diferentes,
-    mp.tipo_confiavel,
-    ST_DISTANCE(mp.posicao_inicial, mp.posicao_final) AS distancia,
-    3.6 * ST_DISTANCE(mp.posicao_inicial, mp.posicao_final) / (TIMESTAMP_DIFF(mp.datahora_final, mp.datahora_inicial, SECOND) + GREATEST(30, mp.erro_datahora)) AS velocidade_media,
-    vv.velocidade_maxima
-FROM movimentacao_placa mp
-INNER JOIN velocidade_veiculo vv
-    ON mp.placa = vv.placa
-WHERE TIMESTAMP_DIFF(datahora_final, datahora_inicial, SECOND) < 300
-
--- Condição incremental
-{% if is_incremental() %}
-  AND mp.datahora_final > COALESCE(
-    (SELECT MAX(datahora_final) FROM {{ this }}), 
-    '2024-05-30'  -- Define uma data padrão antiga para a primeira execução
-  )
-{% endif %}
+SELECT 
+  pv.placa,
+  mc.ano_mes,
+  GREATEST(150, MAX(pv.velocidade) + 30) AS velocidade_maxima,
+  COUNT(pv.placa) AS vezes_vista_total,
+  mc.mediana_primeira_camera,  -- Mediana da primeira câmera ao longo do mês
+  mc.mediana_ultima_camera     -- Mediana da última câmera ao longo do mês
+FROM `rj-civitas.placas_clonadas.dados_radares` pv
+JOIN mediana_cameras mc
+  ON pv.placa = mc.placa AND EXTRACT(YEAR FROM pv.datahora) = mc.ano AND EXTRACT(MONTH FROM pv.datahora) = mc.mes
+GROUP BY pv.placa, mc.ano_mes, mc.ano, mc.mes, mc.mediana_primeira_camera, mc.mediana_ultima_camera;
