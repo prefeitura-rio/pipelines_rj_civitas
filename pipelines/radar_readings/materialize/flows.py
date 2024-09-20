be481245e7e74895a288a7a73366ae02da2f4522
acsamaro <amandacsamaro@gmail.com>
2024-09-20 13:23:08 -0300

    fix: flows.py update group_id to project_name

--- pipelines/radar_readings/materialize/flows.py
+++ pipelines/radar_readings/materialize/flows.py
@@ -13,13 +13,13 @@ from prefeitura_rio.core import settings
 from prefeitura_rio.pipelines_utils.custom import Flow
 from prefeitura_rio.pipelines_utils.prefect import (  # get_flow_run_mode,
     task_get_current_flow_run_labels,
-    task_get_flow_group_id,
 )
 from prefeitura_rio.pipelines_utils.state_handlers import (
     handler_initialize_sentry,
     handler_inject_bd_credentials,
     handler_skip_if_running,
 )
+from prefeitura_rio.pipelines_utils.tasks import get_current_flow_project_name
 
 from pipelines.constants import constants
 from pipelines.radar_readings.materialize.schedules import (
@@ -28,7 +28,7 @@ from pipelines.radar_readings.materialize.schedules import (
 
 # Define the Prefect Flow for data extraction and transformation
 with Flow(
-    name="CIVITAS: radar_readings - Materialização das tabelas",
+    name="CIVITAS: Radar Readings - Materialização das tabelas",
     state_handlers=[
         handler_inject_bd_credentials,
         handler_initialize_sentry,
@@ -41,17 +41,17 @@ with Flow(
     # table_id = Parameter("table_id", default="reports")
     # dbt_alias = Parameter("dbt_alias", default=False)
 
-    materialization_flow_id = task_get_flow_group_id(flow_name=settings.FLOW_NAME_EXECUTE_DBT_MODEL)
     materialization_labels = task_get_current_flow_run_labels()
 
     # dataset_id = dataset_id + "_" + environment if environment != "prod" else dataset_id
-
+    materialization_flow_name = settings.FLOW_NAME_EXECUTE_DBT_MODEL
     dump_prod_tables_to_materialize_parameters = [
         {"dataset_id": dataset_id, "table_id": "previous_next_readings", "dbt_alias": False},
     ]
-
+    current_flow_project_name = get_current_flow_project_name()
     dump_prod_materialization_flow_runs = create_flow_run.map(
-        flow_id=unmapped(materialization_flow_id),
+        flow_name=unmapped(materialization_flow_name),
+        project_name=unmapped(current_flow_project_name),
         parameters=dump_prod_tables_to_materialize_parameters,
         labels=unmapped(materialization_labels),
     )
