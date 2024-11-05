362a0ec799a46d7a40cfb443469331a7baa2e4ee
Nicolas Evilasio <nicolas_evilasio@hotmail.com>
2024-11-05 14:30:14 -0300

    WIP: refactoring pipeline to use vertex api instead of BigQuery

--- pipelines/g20/dbt_run_relatorio_enriquecido/flows.py
+++ pipelines/g20/dbt_run_relatorio_enriquecido/flows.py
@@ -5,41 +5,88 @@ G20 - Alerts flow
 from prefect import Flow, Parameter
 from prefect.run_configs import KubernetesRun
 from prefect.storage import GCS
+
+# from prefect.tasks.prefect import create_flow_run, wait_for_flow_run
+# from prefect.utilities.edges import unmapped
 from prefeitura_rio.pipelines_utils.state_handlers import (
     handler_initialize_sentry,
     handler_inject_bd_credentials,
 )
 
 from pipelines.constants import constants
-from pipelines.g20.dbt_run_relatorio_enriquecido.schedules import g20_reports_schedule
+
+# from pipelines.g20.dbt_run_relatorio_enriquecido.schedules import g20_reports_schedule
 from pipelines.g20.dbt_run_relatorio_enriquecido.tasks import (
-    task_build_messages_text,
-    task_get_secret_folder,
-    task_query_data_from_sql_file,
-    task_send_discord_messages,
-    task_skip_flow_run,
+    task_get_occurrences,
+    task_update_dados_enriquecidos_table,
 )
+
+# from prefeitura_rio.core import settings
+# from prefeitura_rio.pipelines_utils.prefect import task_get_current_flow_run_labels
+# from prefeitura_rio.pipelines_utils.tasks import (
+#     get_current_flow_project_name
+# )
+
 
 with Flow(
     name="CIVITAS: G20 - Alertas",
     state_handlers=[handler_inject_bd_credentials, handler_initialize_sentry],
 ) as g20_alerts:
 
-    data_set_id = Parameter("data_set_id", default="g20")
-    table_id = Parameter("table_id", default="reports_enriquecidos_filtrados")
+    project_id = Parameter("project_id", default="rj-civitas")
+    dataset_id = Parameter("dataset_id", default="integracao_reports")
+    table_id = Parameter("table_id", default="agenda")
+    minutes_interval = Parameter("minutes_interval", default=120)
 
-    secrets: dict = task_get_secret_folder(secret_path="/discord")
+    # secrets: dict = task_get_secret_folder(secret_path="/discord")
 
-    data = task_query_data_from_sql_file(model_dataset_id=data_set_id, model_table_id=table_id)
+    # # Get TEMPLATE flow name
+    # materialization_flow_name = settings.FLOW_NAME_EXECUTE_DBT_MODEL
+    # materialization_labels = task_get_current_flow_run_labels()
+    # current_flow_project_name = get_current_flow_project_name()
 
-    messages = task_build_messages_text(df=data)
-    messages.set_upstream(data)
+    # dump_prod_tables_to_materialize_parameters = [
+    #     {"dataset_id": dataset_id, "table_id": table_id, "dbt_alias": False}
+    # ]
 
-    check_response = task_skip_flow_run(messages)
-    check_response.set_upstream(messages)
+    # dump_prod_materialization_flow_runs = create_flow_run.map(
+    #     flow_name=unmapped(materialization_flow_name),
+    #     project_name=unmapped(current_flow_project_name),
+    #     parameters=dump_prod_tables_to_materialize_parameters,
+    #     labels=unmapped(materialization_labels),
+    # )
 
-    messages_discord = task_send_discord_messages(url_webhook=secrets["G20"], messages=messages)
-    messages_discord.set_upstream(check_response)
+    # dump_prod_materialization_flow_runs.set_upstream(secrets)
+
+    # dump_prod_wait_for_flow_run = wait_for_flow_run.map(
+    #     flow_run_id=dump_prod_materialization_flow_runs,
+    #     stream_states=unmapped(True),
+    #     stream_logs=unmapped(True),
+    #     raise_final_state=unmapped(True),
+    # )
+    occurrences = task_get_occurrences(
+        dataset_id=dataset_id, table_id="reports", minutes_interval=minutes_interval
+    )
+
+    reports_enriquecidos = task_update_dados_enriquecidos_table(
+        df=occurrences,
+        dataset_id=dataset_id,
+        table_id="reports_enriquecidos",
+    )
+
+    reports_enriquecidos.set_upstream(occurrences)
+
+    # reports_enriquecidos_exists = task_check_if_table_exists(dataset_id=dataset_id, table_id='reports_enriquecidos')
+    # data = task_query_data_from_sql_file(model_dataset_id=dataset_id, model_table_id='reports_enriquecidos_v2', minutes_ago=10)
+
+#     messages = task_build_messages_text(df=data)
+#     messages.set_upstream(data)
+
+#     check_response = task_skip_flow_run(messages)
+#     check_response.set_upstream(messages)
+
+#     messages_discord = task_send_discord_messages(url_webhook=secrets["G20"], messages=messages)
+#     messages_discord.set_upstream(check_response)
 
 g20_alerts.storage = GCS(constants.GCS_FLOWS_BUCKET.value)
 g20_alerts.run_config = KubernetesRun(
@@ -49,4 +96,4 @@ g20_alerts.run_config = KubernetesRun(
     ],
 )
 
-g20_alerts.schedule = g20_reports_schedule
+# g20_alerts.schedule = g20_reports_schedule
--- pipelines/g20/dbt_run_relatorio_enriquecido/tasks.py
+++ pipelines/g20/dbt_run_relatorio_enriquecido/tasks.py
@@ -3,6 +3,10 @@
 This module contains tasks for appending new data to Google Sheets.
 """
 import asyncio
+import concurrent.futures
+import json
+
+# import time
 from datetime import datetime, timedelta
 from typing import List, Literal
 
@@ -17,6 +21,12 @@ from prefeitura_rio.pipelines_utils.infisical import get_secret_folder
 from prefeitura_rio.pipelines_utils.io import get_root_path
 from prefeitura_rio.pipelines_utils.logging import log
 
+from pipelines.g20.dbt_run_relatorio_enriquecido.utils import (  # ml_generate_text,; query_data_from_sql_file,
+    check_if_table_exists,
+    get_delay_time_string,
+    load_data_from_dataframe,
+    safe_generate_content,
+)
 from pipelines.utils import send_discord_message
 
 bd.config.billing_project_id = "rj-civitas"
@@ -24,15 +34,256 @@ bd.config.from_file = True
 tz = pytz.timezone("America/Sao_Paulo")
 
 
+# use bd.read_sql to get the table integrations from
+# rj-civitas.integracao_reports.reports
 @task
-def task_query_data_from_sql_file(model_dataset_id: str, model_table_id: str) -> pd.DataFrame:
-    log(f"Querying data from {model_dataset_id}.{model_table_id}")
-    root_path = get_root_path()
-    model_path = root_path / f"queries/models/{model_dataset_id}/{model_table_id}.sql"
-    query = model_path.read_text(encoding="utf-8")
+def task_get_occurrences(
+    dataset_id: str, table_id: str, minutes_interval: int = 30
+) -> pd.DataFrame:
+    try:
+        minutes_interval = int(minutes_interval)
+    except Exception as e:
+        raise ValueError(f"{e} - minutes_interval must be an integer")
+
+    query = rf"""select
+            ifnull(id_report, '') as id_report,
+            ifnull(id_source, '') as id_source,
+            ifnull(id_report_original, '') as id_report_original,
+            ifnull(
+                datetime(data_report, 'America/Sao_Paulo'), cast('' as datetime)
+            ) as data_report,
+            ifnull(
+                array(select ifnull(orgao, '') from unnest(orgaos) as orgao), []
+            ) as orgaos,
+            ifnull(categoria, '') as categoria,
+            array(
+                select
+                    struct(
+                        ifnull(item.tipo, '') as tipo,
+                        ifnull(
+                            array(
+                                select ifnull(sub, '') from unnest(item.subtipo) as sub
+                            ),
+                            []
+                        ) as subtipo
+                    )
+                from unnest(tipo_subtipo) as item
+            ) as tipo_subtipo,
+            ifnull(descricao, '') as descricao,
+            ifnull(logradouro, '') as logradouro,
+            ifnull(numero_logradouro, '') as numero_logradouro,
+            ifnull(latitude, cast(0 as float64)) as latitude,
+            ifnull(longitude, cast(0 as float64)) as longitude
+        from `rj-civitas.{dataset_id}.{table_id}` tablesample system(10 percent)
+        where
+            datetime(data_report, 'America/Sao_Paulo') >= timestamp_sub(
+                datetime(current_timestamp(), 'America/Sao_Paulo'), interval {minutes_interval} minute
+            )
+            and id_source = 'DD'"""
+
     df = bd.read_sql(query)
 
     return df
+
+
+@task
+def task_check_if_table_exists(dataset_id: str, table_id: str) -> bool:
+    return check_if_table_exists(dataset_id, table_id)
+
+
+@task
+def task_update_dados_enriquecidos_table(
+    df: pd.DataFrame, dataset_id: str, table_id: str, model_name: str = "gemini-1.5-flash"
+) -> None:
+    target_table_exists = check_if_table_exists(
+        dataset_id=dataset_id,
+        table_id=table_id,
+    )
+
+    log(f">>>>>>>>>>>>>> {target_table_exists}")
+
+    if len(df) > 0:
+        log(f"Creating {len(df)} prompts...")
+        df["prompt"] = df.apply(
+            lambda row: """
+            Você é um analista de segurança especializado no evento do G20.
+            Sua função é avaliar ocorrências e classificar o risco potencial para a vida e integridade
+            física dos participantes.
+            Avalie o risco independentemente da localização exata do evento ou ocorrência.
+            Forneça justificativas claras e objetivas para cada classificação, e preencha todos os campos
+            do JSON com precisão.
+            Siga as instruções passo a passo.
+
+            Para cada ocorrência, siga as instruções abaixo:
+
+            1. **Tópicos**:
+                - Identifique o tópico principal e quaisquer tópicos relacionados com base na descrição da ocorrência.
+                - Exemplos de tópicos principais incluem: “ameaça à segurança”, “condições climáticas adversas”,
+                “protestos”, “problemas de infraestrutura”, se necessário, adicione tópicos relacionados
+                para complementar a classificação.
+                - Justifique a escolha do tópico principal e dos relacionados com base na descrição do evento.
+
+            2. **Nível de Abrangência**:
+                - Classifique o nível de impacto com uma justificativa detalhada, usando uma das
+                categorias a seguir:
+                    - **Casa**: Afeta apenas uma residência/estabelecimento
+                    - **Quadra**: Impacto limitado à área da quadra
+                    - **Bairro**: Afeta um bairro inteiro
+                    - **Região**: Impacta múltiplos bairros
+                    - **Cidade**: Afeta toda a cidade
+                    - **Estado**: Impacto em nível estadual
+                    - **País**: Repercussão nacional
+                - Descreva o motivo da escolha do nível de abrangência com base no escopo potencial de impacto.
+
+            3. **Avaliação Temporal**:
+                - Defina o intervalo de tempo em minutos até o possível início da ocorrência,
+                explicando a estimativa com base nos dados disponíveis.
+                - Use “0” para tempos indefinidos.
+                - Explique como chegou à estimativa para o horário previsto, considerando as informações
+                fornecidas.
+
+            4. **Nível de Ameaça**:
+                - Avalie o risco e escolha entre os níveis abaixo:
+                    - **BAIXO**: Risco indireto ou muito improvável (não representa risco direto à
+                    vida ou integridade, inclui maus-tratos a animais, transgressões ambientais,
+                    trabalhistas, etc.)
+                    - **ALTO**: Ameaça iminente à vida ou integridade física (inclui tiroteios,
+                    bloqueio de vias, manifestações, ameaças de bombas ou terrorismo).
+                - Justifique a avaliação da ameaça com uma análise objetiva do risco envolvido,
+                considerando o potencial de dano à vida e integridade física dos participantes.
+
+            Ocorrencia :
+            ID da Ocorrência: {id_report}
+
+            Data do Relatório (Quando a denúncia chegou à prefeitura): cast({data_report} as string)
+            Categoria: {categoria}
+            Tipo/Subtipo: to_json_string({tipo_subtipo})
+
+            Organizações: to_json_string({orgaos})
+            Descrição: {descricao}
+
+            Retorne apenas os seguintes campos em JSON nomeados exatamente como abaixo:
+            {{
+                "id_report": "{id_report}",
+                "main_topic": "tópico principal",
+                "related_topics": ["array de tópicos relacionados"],
+                "scope_level_explanation": "Explicação para o nível de abrangência",
+                "scope_level": "nível de abrangência",
+                "predicted_time_explanation": "Explicação para os horários previstos",
+                "predicted_time_interval": "valor horário previsto",
+                "threat_explanation": "Avaliação detalhada da ameaça",
+                "threat_level": "valor nível de ameaça"
+            }}
+            **Instrução adicional**: Ao retornar os dados em JSON, **escape** corretamente todos os
+            caracteres especiais, como aspas duplas (`"`), barras invertidas (`\\`), traços (`-`),
+            e outros caracteres que podem afetar a sintaxe do JSON.
+            **Não altere** a acentuação ou os caracteres normais da língua portuguesa.
+            """.format(
+                id_report=row["id_report"],
+                data_report=row["data_report"],
+                categoria=row["categoria"],
+                tipo_subtipo=row["tipo_subtipo"],
+                orgaos=row["orgaos"],
+                descricao=row["descricao"],
+            ),
+            axis=1,
+        )
+
+        if target_table_exists:
+            query = f"""SELECT id_report, prompt FROM `rj-civitas.{dataset_id}.{table_id}`"""
+            old_ids = bd.read_sql(query)
+
+            data = df.loc[
+                (~df["id_report"].isin(old_ids["id_report"]))
+                & (~df["prompt"].isin(old_ids["prompt"]))
+            ]
+        else:
+            data = df
+
+        if len(data) > 0:
+            log(f"Loading {len(data)} rows to {dataset_id}.{table_id}")
+
+            antes = datetime.now()
+            final_responses = []
+            prompts = [(model_name, row["prompt"]) for _, row in data.iterrows()]
+
+            with concurrent.futures.ThreadPoolExecutor() as executor:
+                futures = [
+                    executor.submit(safe_generate_content, model, prompt)
+                    for model, prompt in prompts
+                ]
+
+                for future in concurrent.futures.as_completed(futures):
+                    response = future.result()
+                    final_responses.append(response)
+
+            # load_data_from_dataframe(data, dataset_id, table_id)
+            # ml_generate_text(data, final_responses, prompts)
+            depois = datetime.now()
+
+            log(f">>>>>>>>>> Tempo total: {depois - antes}\n")
+            # log(final_responses[0])
+            # final_responses
+
+            dados_dict = []
+            for json_string in final_responses:
+                try:
+                    if "```json" in json_string:
+                        # Remover a marcação e extrair a parte JSON
+                        json_part = json_string.split("```json")[1].split("```")[0].strip()
+
+                        # Converte a string JSON em um dicionário
+                        dados_dict.append(json.loads(json_part))
+                    else:
+                        log(f" JSON STRING BUGADA >>>>>>>>: {json_string}")
+                        dados_dict.append(json.loads(json_string))
+
+                except (IndexError, json.JSONDecodeError) as e:
+                    log(f"Error processing json string: {json_string}\nError: {e}")
+
+            # log(dados_dict[0])
+            enriched_data = pd.DataFrame(dados_dict)
+
+            final_df = pd.merge(data, enriched_data, on="id_report", how="left")
+
+            load_data_from_dataframe(final_df, dataset_id, table_id)
+        else:
+            log(f"No new data to load to {dataset_id}.{table_id}")
+
+
+@task
+def task_query_data_from_sql_file(
+    model_dataset_id: str, model_table_id: str, minutes_ago: int = 30
+) -> pd.DataFrame:
+    log(f"Querying data from {model_dataset_id}.{model_table_id}")
+
+    try:
+        minutes_ago = int(minutes_ago)
+    except Exception as e:
+        raise ValueError(f"{e} - minutes_ago must be an integer")
+
+    root_path = get_root_path()
+    model_path = root_path / f"queries/models/{model_dataset_id}/{model_table_id}.sql"
+    query = model_path.read_text(encoding="utf-8")
+    final_query = query.replace("interval 30 minute", f"interval {minutes_ago} minute")
+    # query = f"""SELECT * FROM {dataset_id}.{table_id}"""
+
+    df = bd.read_sql(final_query)
+
+    return df
+
+
+# @task
+# def filter_new_data(df: pd.DataFrame, dataset_id: str, table_id: str) -> pd.DataFrame:
+#     query = f"""SELECT id FROM {dataset_id}.{table_id}"""
+#     path = root_path / f"queries/models/{dataset_id}/{table_id}.sql"
+#     query = model_path.read_text(encoding="utf-8")
+#     # query = f"""SELECT * FROM {dataset_id}.{table_id}"""
+
+#     df = bd.read_sql(query)
+
+#     return df
+# query = f""""""
 
 
 @task
@@ -42,52 +293,6 @@ def task_skip_flow_run(data):
         log(message)
         skip = Skipped(message=message)
         raise ENDRUN(state=skip)
-
-
-def get_delay_time_string(df_ocorrencias: pd.DataFrame, datetime_column: str):
-    """
-    Returns a string with the time difference between the current datetime and the datetime
-    in the 'data_ocorrencia' column of the given dataframe.
-
-    Args:
-        df_ocorrencias (pd.DataFrame): The dataframe with the 'data_ocorrencia' column.
-
-    Returns:
-        str: A string with the time difference (e.g. "3 dias, 2 horas, 1 minuto e 2 segundos").
-    """
-    occurrence_datetime = df_ocorrencias[datetime_column]
-    delta = datetime.now(tz=tz) - occurrence_datetime.tz_localize(tz)
-
-    days = delta.days
-    hours, remainder = divmod(delta.seconds, 3600)
-    minutes, seconds = divmod(remainder, 60)
-
-    # Creating a list to store parts of the time string
-    time_parts = []
-
-    # Function to add time parts to the list
-    def add_time_part(value, singular, plural):
-        if value == 1:
-            time_parts.append(f"{value} {singular}")
-        elif value > 1:
-            time_parts.append(f"{value} {plural}")
-
-    # Adding parts for days, hours, minutes, and seconds
-    add_time_part(days, "dia", "dias")
-    add_time_part(hours, "hora", "horas")
-    add_time_part(minutes, "minuto", "minutos")
-    add_time_part(seconds, "segundo", "segundos")
-
-    # Joining parts with commas and "and" for the last element
-    if time_parts:
-        if len(time_parts) == 1:
-            time_string = time_parts[0]  # Only one element
-        else:
-            time_string = ", ".join(time_parts[:-1]) + " e " + time_parts[-1]
-    else:
-        time_string = "0 segundos"
-
-    return time_string
 
 
 @task
