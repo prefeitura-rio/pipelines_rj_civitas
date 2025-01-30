e74d6a1d9e09560917a28c4e6e8696bfb2879675
nicolasevilasio <nicolas_evilasio@hotmail.com>
2025-01-30 13:03:28 -0300

    feat: implement detailed error logging system
    - Add new handler to save traceback information
    - Store detailed error logs in /tmp/pipelines/error_logs
    - Enhance Discord notifications with full error details
    - Format error messages with markdown and code blocks

--- pipelines/utils/state_handlers.py
+++ pipelines/utils/state_handlers.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 import asyncio
 import os
+from pathlib import Path
+from textwrap import dedent
+import traceback
 
 from prefect import Flow, Task, context
 from prefect.engine import state
@@ -8,6 +11,32 @@ from prefect.engine import state
 from pipelines.utils.notifications import send_discord_message
 
 
+def handler_save_traceback_on_failure(obj, old_state, new_state):
+    if isinstance(new_state, state.Failed):
+        exc_info = (type(new_state.result), new_state.result, new_state.result.__traceback__)
+        full_traceback = ''.join(traceback.format_exception(*exc_info))
+        
+        task_message = str(new_state.message)
+        task_cached_inputs = str(new_state.cached_inputs)
+        
+        error_description = dedent(f"""\
+            **Task message:**
+            {task_message}
+
+            **Task cached inputs:**
+            {task_cached_inputs}
+            """
+        )       
+        error_description += f"\n```bash\n{full_traceback}\n```"
+        
+        file_dir = Path('/tmp/pipelines/error_logs')
+        file_dir.mkdir(parents=True, exist_ok=True)
+        file_name = f'{context.get("flow_run_id")}.txt'
+        
+        with open(file_dir / file_name, 'w') as f:
+            f.write(str(error_description))
+
+        
 def handler_notify_on_failure(obj: Flow | Task, old_state: state.State, new_state: state.State):
     """
     State handler that sends a notification to Discord when a Flow or Task fails.
@@ -28,15 +57,22 @@ def handler_notify_on_failure(obj: Flow | Task, old_state: state.State, new_stat
 
         flow_run_url = f"{server_url}/{project_name}/flow-run/{flow_run_id}?logs"
 
-        new_state_message = f"{new_state.message}\n"
+        new_state_message = f"**{new_state.message}**\n"
         new_state_message += f"**Flow:** {flow_name}\n"
         new_state_message += f"**URL:** {flow_run_url}\n"
+        
+        file_name = f'{context.get("flow_run_id")}.txt'
+        log_file_path = Path('/tmp/pipelines/error_logs') / file_name
+        
+        if os.path.exists(log_file_path):
+            with open(log_file_path, 'r') as f:
+                error_description = f.read()
+                new_state_message += f"\n{error_description}"
 
         asyncio.run(
             send_discord_message(
                 webhook_url=os.getenv("PIPELINES_RESULTS"),
                 message=new_state_message,
-                # file=file_bytes,
-                # file_format="txt"
+                username="Prefect"
             )
         )
