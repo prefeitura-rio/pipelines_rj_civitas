0c352f7847548bb71e4dc7ecaad6db74e0a759ba
nicolas evilasio <nicolas_evilasio@hotmail.com>
2025-04-28 11:07:35 -0300

    refactor: implement incremental model to handle updates and new inserts

--- queries/models/cerco_digital/licenciamento_veiculos.sql
+++ queries/models/cerco_digital/licenciamento_veiculos.sql
@@ -1,6 +1,6 @@
 {{
     config(
-        materialized='table',
+        materialized='incremental',
         partition_by={
             "field": "datetime_ultima_atualizacao",
             "data_type": "datetime",
@@ -22,6 +22,11 @@ WITH base_query AS (
     ROW_NUMBER() OVER(PARTITION BY placa ORDER BY datetime_ultima_atualizacao DESC) rn
   FROM
     {{ source('smtr_veiculo', 'licenciamento') }}
+  {% if is_incremental() %}
+  WHERE
+    datetime_ultima_atualizacao > (SELECT MAX(datetime_ultima_atualizacao) FROM {{ this }})
+    OR placa NOT IN (SELECT DISTINCT placa FROM {{ this }})
+  {% endif %}
   QUALIFY rn = 1
 )
 SELECT * EXCEPT(rn) FROM base_query
