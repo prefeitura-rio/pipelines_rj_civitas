364a9bd34f9c7960d9292ecbc0100e0b71897b04
nicolasevilasio <nicolas_evilasio@hotmail.com>
2025-09-11 16:32:47 -0300

    fix: import flow explicitly instead of wildcard

--- pipelines/cerco_digital/__init__.py
+++ pipelines/cerco_digital/__init__.py
@@ -6,4 +6,4 @@ module.
 
 from pipelines.cerco_digital.extract_load.flows import *  # noqa
 from pipelines.cerco_digital.readings.flows import *  # noqa
-from pipelines.cerco_digital.cameras.flows import *  # noqa
+from pipelines.cerco_digital.cameras.flows import elt_cerco_digital_cameras  # noqa
