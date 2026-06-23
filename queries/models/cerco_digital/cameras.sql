f60330a0cf609954d6117fe3f66416d5b3aa380a
Jorge Mendes <jserpamendes@gmail.com>
2026-06-23 15:33:12 -0300

    feat: acrescentar source cameras_tixxi_indoor e coluna booleana camera_interna na tabela cameras

--- queries/models/cerco_digital/cameras.sql
+++ queries/models/cerco_digital/cameras.sql
@@ -7,18 +7,21 @@
 WITH tixxi_base_cleaned AS (
   -- Standardizing data from tixxi cameras table
   SELECT
-    TRIM(CameraCode) AS codigo_camera,
-    INITCAP(TRIM(CameraName)) AS nome_camera,
-    INITCAP(TRIM(CameraZone)) AS zona_camera,
-    SAFE_CAST(Latitude AS FLOAT64) AS latitude,
-    SAFE_CAST(Longitude AS FLOAT64) AS longitude,
-    REPLACE(REPLACE(TRIM(Streamming), 'app', 'dev'), 'outvideo', 'outvideo3') AS streaming_url,
+    TRIM(a.CameraCode) AS codigo_camera,
+    INITCAP(TRIM(a.CameraName)) AS nome_camera,
+    INITCAP(TRIM(a.CameraZone)) AS zona_camera,
+    SAFE_CAST(a.Latitude AS FLOAT64) AS latitude,
+    SAFE_CAST(a.Longitude AS FLOAT64) AS longitude,
+    REPLACE(REPLACE(TRIM(a.Streamming), 'app', 'dev'), 'outvideo', 'outvideo3') AS streaming_url,
     'TIXXI' AS sistema_origem,
-    'COR' AS responsavel
-  FROM {{ source('stg_cerco_digital', 'cameras') }}
-  WHERE CameraCode IS NOT NULL
+    'COR' AS responsavel,
+    b.codigo_tixxi IS NOT NULL AS camera_interna
+  FROM {{ source('stg_cerco_digital', 'cameras') }} a
+  LEFT JOIN {{ source('stg_cerco_digital', 'cameras_tixxi_indoor')}} b
+  ON SAFE_CAST(a.CameraCode AS INT64) = SAFE_CAST(b.codigo_tixxi AS INT64)
+  WHERE a.CameraCode IS NOT NULL
   -- Deduplicate based on codigo_camera at source level
-  QUALIFY ROW_NUMBER() OVER(PARTITION BY CameraCode ORDER BY Latitude DESC) = 1
+  QUALIFY ROW_NUMBER() OVER(PARTITION BY a.CameraCode ORDER BY Latitude DESC) = 1
 ),
 
 civitas_cameras_vms AS (
@@ -59,9 +62,10 @@ civitas_streaming_url AS (
     a.zona_camera,
     a.latitude,
     a.longitude,
-    IF (b.entity_id IS NULL, CAST(NULL AS STRING), CONCAT('{{ env_var("HEXAGON_VMS__RTSP_URL_PREFIX") }}', b.entity_id)) AS streaming_url,
+    IF (b.entity_id IS NULL, CAST(NULL AS STRING), CONCAT('{{ env_var("HEXAGON_VMS__RTSP_URL_PREFIX", "dummy") }}', b.entity_id)) AS streaming_url,
     a.sistema_origem,
-    a.responsavel
+    a.responsavel,
+    FALSE AS camera_interna
   FROM civitas_base_cleaned a
   JOIN {{ ref('camera') }} b USING (codigo_camera)
 ),
@@ -88,7 +92,8 @@ SELECT
   END AS longitude,
   streaming_url,
   sistema_origem,
-  responsavel
+  responsavel,
+  camera_interna
 FROM merged_cameras
 WHERE
   codigo_camera IS NOT NULL
