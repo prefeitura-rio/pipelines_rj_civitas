6c22422080161517937d89c1753af722a16f8fdf
Jorge Mendes <jserpamendes@gmail.com>
2026-07-23 11:49:59 -0300

    feat: change hexagon/camera to adapt to the new raw camera table

--- queries/models/hexagon/camera.sql
+++ queries/models/hexagon/camera.sql
@@ -9,5 +9,9 @@
     SAFE_CAST(can_ptz AS BOOLEAN) AS can_ptz,
     SAFE_CAST(entity_id AS STRING) AS entity_id,
     SAFE_CAST(entity_name AS STRING) AS entity_name,
-    SAFE_CAST(codigo_camera AS STRING) AS codigo_camera
+    SAFE_CAST(codigo_camera AS STRING) AS codigo_camera,
+    timestamp_insercao
   FROM {{ source('hexagon_staging', 'camera') }}
+  WHERE timestamp_insercao >= TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL 45 DAY)
+  QUALIFY ROW_NUMBER() OVER(PARTITION BY entity_id ORDER BY timestamp_insercao DESC) = 1 
+          AND ROW_NUMBER() OVER(PARTITION BY codigo_camera ORDER BY timestamp_insercao DESC) = 1 
