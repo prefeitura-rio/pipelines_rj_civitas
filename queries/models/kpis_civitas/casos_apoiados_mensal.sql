9bd14f6707f222b4dd51df8ed822cadc7970a236
Jorge Mendes <jserpamendes@gmail.com>
2026-08-14 11:16:35 -0300

    fix: mes by data_entrada and filter invalid data

--- queries/models/kpis_civitas/casos_apoiados_mensal.sql
+++ queries/models/kpis_civitas/casos_apoiados_mensal.sql
@@ -7,41 +7,43 @@
 WITH old_tickets AS (
   SELECT
     numero_referencia,
-    data_saida
+    data_entrada
   FROM {{ source('stg_cerco_digital', 'controle_demandas_operacional_2024') }}
 
   UNION ALL
 
   SELECT
     numero_referencia,
-    data_saida
+    data_entrada
   FROM {{ source('stg_cerco_digital', 'controle_demandas_operacional_2025') }}
   
   UNION ALL
 
   SELECT
     numero_referencia,
-    data_saida
+    data_entrada
   FROM {{ source('stg_cerco_digital', 'controle_demandas_operacional_2026') }}
 ),
 
 all_tickets AS (
   SELECT
     UPPER(TRIM(numero_referencia)) AS id,
-    SAFE.PARSE_DATE('%d/%m/%Y', TRIM(data_saida)) AS data_saida
+    SAFE.PARSE_DATE('%d/%m/%Y', TRIM(data_entrada)) AS data_entrada
   FROM old_tickets
 
   UNION ALL
 
   SELECT
     CAST(id AS STRING) AS id,
-    DATE(completed_at) AS data_saida
+    DATE(created_at) AS data_entrada
   FROM {{ source('brutos_app_civitas', 'tickets')}}
+  WHERE _ab_cdc_deleted_at IS NULL
 )
 
 SELECT
   COUNT(DISTINCT(id)) as casos_apoiados,
-  DATE_TRUNC(data_saida, MONTH) as mes
+  DATE_TRUNC(data_entrada, MONTH) as mes
   FROM all_tickets
+  WHERE COALESCE(data_entrada, DATE('1970-01-01')) >= '2024-06-01'
 group by mes
 order by mes
